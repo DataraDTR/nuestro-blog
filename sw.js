d0fb29684ac6a04817a97dcc6cb6ce25671a6a9a
midatara <datara.host@gmail.com>
2025-10-17 10:54:49 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "galleries-amor-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/script.js",
+    "/manifest.json",
+    "/heart_icon.png",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+];
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
+});
+
+self.addEventListener("activate", e => {
+    e.waitUntil(
+        caches.keys().then(keys => 
+            Promise.all(
+                keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+            )
+        )
+    );
+});
